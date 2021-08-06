428bd80c9218d6a3fb1a2c414147e5ef75bc50e0
everybodyneedsis <jacobkylefranklin@gmail.com>
2021-08-06 15:54:49 -0400

    setting up codegrade

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,26 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+
+  const header = document.createElement('div');
+  const headerDate = document.createElement('span');
+  const headerTitle = document.createElement('h1');
+  const headerTemp = document.createElement('span');
+
+  header.classList.add("header");
+  headerDate.classList.add("date");
+  headerTemp.classList.add("temp");
+
+  headerDate.textContent = date;
+  headerTitle = title;
+  headerTemp = temp;
+
+  header.appendChild(headerDate);
+  header.appendChild(headerTitle);
+  header.appendChild(headerTemp);
+
+  return header;
+
 }
 
 const headerAppender = (selector) => {
@@ -23,3 +43,9 @@ const headerAppender = (selector) => {
 }
 
 export { Header, headerAppender }
+
+
+
+
+
+
